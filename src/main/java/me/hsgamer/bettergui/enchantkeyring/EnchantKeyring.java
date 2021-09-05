015042d42e9f8d11b89f0e97c7e9c3d12c4f213d
Huynh Tien <huynhqtienvtag@gmail.com>
2021-09-05 21:21:01 +0700

    Init

---
+++ src/main/java/me/hsgamer/bettergui/enchantkeyring/EnchantKeyring.java
@@ -0,0 +1,10 @@
+package me.hsgamer.bettergui.enchantkeyring;
+
+import me.hsgamer.bettergui.api.addon.BetterGUIAddon;
+
+public final class EnchantKeyring extends BetterGUIAddon {
+    @Override
+    public void onEnable() {
+        // Enable logic
+    }
+}
