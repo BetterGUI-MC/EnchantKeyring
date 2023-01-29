7c9776f2162e3145a9a1f9d0f9dab1519e642bbf
HSGamer <huynhqtienvtag@gmail.com>
2023-01-29 20:51:08 +0700

    check before enable

--- src/main/java/me/hsgamer/bettergui/enchantkeyring/EnchantKeyring.java
+++ src/main/java/me/hsgamer/bettergui/enchantkeyring/EnchantKeyring.java
@@ -2,8 +2,21 @@ package me.hsgamer.bettergui.enchantkeyring;
 
 import me.hsgamer.bettergui.builder.ItemModifierBuilder;
 import me.hsgamer.hscore.bukkit.addon.PluginAddon;
+import me.hsgamer.hscore.common.Validate;
 
 public final class EnchantKeyring extends PluginAddon {
+    @Override
+    public boolean onLoad() {
+        Class<?> namespaceKeyClass;
+        try {
+            namespaceKeyClass = Class.forName("org.bukkit.NamespacedKey");
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
+
+        return Validate.isMethodLoaded("org.bukkit.enchantments.Enchantment", "getByKey", namespaceKeyClass);
+    }
+
     @Override
     public void onEnable() {
         ItemModifierBuilder.INSTANCE.register(CustomEnchantmentModifier::new, "custom-enchantment", "custom-enchant", "enchantment-key", "enchant-key");
